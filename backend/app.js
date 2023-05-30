c1a1179268238bb39c020878d534db00fabe67bd
Dejvi Dervishi <dimiderv@gmail.com>
2023-05-30 01:34:13 -0400

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,229 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const axios = require("axios").default;
+const Goal = require("./models/goal");
+const app = express();
+// required for registration
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require('./models/userMode');
+const auth = require('./auth');
+// Could be a possible error with new Header
+var cors = require("cors");
+app.use(cors());
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  { flags: "a" }
+);
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE,PUT, PATCH, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  next();
+});
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+
+
+/*===============================Login and Registration of users======================*/
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch erroe if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+
+          // check if password matches
+          if(!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password do not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+// free endpoint
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.send({ message: "You are authorized to access me" });
+});
+
+/**====================================End of Authentication===========================**/
+app.get("/goals", async (req, res) => {
+  console.log("TRYING TO FETCH GOALS");
+  try {
+    const goals = await Goal.find();
+    res.status(200).json({
+      goals: goals.map((goal) => ({
+        id: goal.id,
+        text: goal.text,
+      })),
+    });
+    console.log("FETCHED GOALS");
+  } catch (err) {
+    console.error("ERROR FETCHING GOALS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to load goals." });
+  }
+});
+
+app.post("/goals", async (req, res) => {
+  console.log("TRYING TO STORE GOAL");
+  const goalText = req.body.text;
+
+  if (!goalText || goalText.trim().length === 0) {
+    console.log("INVALID INPUT - NO TEXT");
+    return res.status(422).json({ message: "Invalid goal text." });
+  }
+
+  const goal = new Goal({
+    text: goalText,
+  });
+
+  try {
+    await goal.save();
+    res
+      .status(201)
+      .json({ message: "Goal saved", goal: { id: goal.id, text: goalText } });
+    console.log("STORED NEW GOAL");
+  } catch (err) {
+    console.error("ERROR FETCHING GOALS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to save goal." });
+  }
+});
+
+app.delete("/goals/:id", async (req, res) => {
+  console.log("TRYING TO DELETE GOAL");
+  try {
+    await Goal.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: "Deleted goal!" });
+    console.log("DELETED GOAL");
+  } catch (err) {
+    console.error("ERROR FETCHING GOALS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to delete goal." });
+  }
+});
+
+mongoose.connect(
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error("FAILED TO CONNECT TO MONGODB");
+      console.error(err);
+    } else {
+      console.log("CONNECTED TO MONGODB!!");
+      app.listen(80);
+    }
+  }
+);
---
+++ backend/models/userMode.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// user schema
+const UserSchema = new mongoose.Schema({
+  // email field
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+
+  //   password field
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
