bb842929f86117bc60239efdea51199963bd4636
dimiderv <dimiderv@gmail.com>
2024-01-05 01:18:08 -0500

    Added Toaster component, still unfinished

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from "react"
 import Social from "./Social";
-import { Row } from "react-bootstrap";
-const Footer = () => <Row className="justify-content-md-center"> <footer className="page-footer font-small blue pt-4">
- 
+import {Row} from "react-bootstrap";
 
- 
+const Footer = () =>
 
-    <Social />
-
-
-</footer>
-</Row>
+    <Row className="justify-content-md-center">
+        <footer className="page-footer font-small blue pt-4">
+            <Social/>
+        </footer>
+    </Row>
 
 export default Footer
--- frontend/src/components/templates/Product.js
+++ frontend/src/components/templates/Product.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Card, Button, Row} from "react-bootstrap";
+import {Card, Button, Row, Toast, Col} from "react-bootstrap";
 import MyModal from "./MyModal";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar, faHeart, faThumbsDown} from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +10,7 @@ export default function Product(props) {
     const [heartClicked, setHeartClicked] = useState(false);
     const [starClicked, setStarClicked] = useState(false);
     const [dislikeClicked, setDislikeClicked] = useState(false)
+    const [show, setShow] = useState(false)
 
     function handleModal() {
         setActive(!active);
@@ -19,57 +20,91 @@ export default function Product(props) {
         <MyModal show={active} onHide={handleModal} movie={props.movie}/>
     ) : (
         // bg={'Dark'} style={{ width: "18rem", margin: "10px",background:'black'}}
-        <Card>
-            <Card.Img
-                variant="top"
-                src={props.thumbnail}
-                alt={props.title}
-                width={259}
-                height={380}
-                title={props.title}
-            />
+        <div>
+            {/*I have to fix this error, message looks fine for now, the idea is looking good*/}
+            <div
+                style={{
+                    position: 'fixed',
+                    bottom: '0',
+                    right: '0',
+                    padding: '1rem', // Adjust padding as needed
+                    zIndex: 9999, // Make sure it's above other elements
+                }}
+            >
+                <Row>
+                    <Col xs={12}>
+                        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+                            <Toast.Header>
 
-            {/* <Card.Body>
+
+                                    <strong className="mr-auto">Bootstrap</strong>
+                                    <small>11 mins ago</small>
+
+                            </Toast.Header>
+                            <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
+                        </Toast>
+                    </Col>
+                    {/*<Col xs={6}>*/}
+                    {/*    /!*<Button onClick={() => setShow(true)}>Show Toast</Button>*!/*/}
+                    {/*</Col>*/}
+                </Row>
+            </div>
+            <Card>
+                <Card.Img
+                    variant="top"
+                    src={props.thumbnail}
+                    alt={props.title}
+                    width={259}
+                    height={380}
+                    title={props.title}
+                />
+
+                {/* <Card.Body>
       <Card.Title>{title}</Card.Title>
 
       <Button style={{justifyContent:'left'}}variant="primary" type='submit' value="Send" onClick={()=>addGoalHandler(title)}>Post</Button>
         <Button className='ml-auto'style={{justifyContent:'right'}}variant="primary" type='submit' value="Send" onClick={()=>addGoalHandler(title)}>More</Button>
     </Card.Body> */}
-            <Card.Body className={"favBody "}>
+                <Card.Body className={"favBody "}>
 
 
-                <Row>
-                    <div className={"col-auto"}>
-                        <Button
-                            size="sm"
-                            variant="primary"
-                            type="submit"
-                            value="Send"
-                            onClick={() => setActive(!active)}
-                            title={"Click for more information"}
-                        >More
-                        </Button>
-                    </div>
-                    <div className={"col-0"}></div>
+                    <Row>
+                        <div className={"col-auto"}>
+                            <Button
+                                size="sm"
+                                variant="primary"
+                                type="submit"
+                                value="Send"
+                                onClick={() => setActive(!active)}
+                                title={"Click for more information"}
+                            >More
+                            </Button>
+                        </div>
+                        <div className={"col-0"}></div>
 
-                    <div className={"col-auto"}>
+                        <div className={"col-auto"}>
 
-                        <FontAwesomeIcon icon={faHeart} className={!heartClicked ? 'favHeart m-2' : 'favHeartLike m-2'}
-                                         onClick={() => setHeartClicked(!heartClicked)} size="xl"
-                                         title={"Add to Favorites"}/>
+                            <FontAwesomeIcon icon={faHeart}
+                                             className={!heartClicked ? 'favHeart m-2' : 'favHeartLike m-2'}
+                                             onClick={() => {
+                                                 setShow(true)
+                                                 setHeartClicked(!heartClicked)
+                                             }} size="xl"/>
 
-                        <FontAwesomeIcon icon={faStar} className={!starClicked ? 'star m-2' : 'starClicked m-2'}
-                                         onClick={() => setStarClicked(!starClicked)} size="xl" title={"Star"}/>
-                        <FontAwesomeIcon icon={faThumbsDown}
-                                         className={!dislikeClicked ? 'dislike m-2' : 'dislikeClicked m-2'}
-                                         onClick={() => setDislikeClicked(!dislikeClicked)} size="xl" title={"Dislike"}/>
-                    </div>
-                </Row>
+                            <FontAwesomeIcon icon={faStar} className={!starClicked ? 'star m-2' : 'starClicked m-2'}
+                                             onClick={() => setStarClicked(!starClicked)} size="xl" title={"Star"}/>
+                            <FontAwesomeIcon icon={faThumbsDown}
+                                             className={!dislikeClicked ? 'dislike m-2' : 'dislikeClicked m-2'}
+                                             onClick={() => setDislikeClicked(!dislikeClicked)} size="xl"
+                                             title={"Dislike"}/>
+                        </div>
+                    </Row>
 
 
-            </Card.Body>
+                </Card.Body>
 
-        </Card>
+            </Card>
+        </div>
     );
 
 }
